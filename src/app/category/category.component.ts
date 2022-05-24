2133fc1c400cd3654aa086112d9d140f531b5505
José Antonio Portuguez Vázquez <65097050+Vapss@users.noreply.github.com>
2022-05-24 09:39:03 -0500

    Clase 24 de Mayo
    Clase 24 de Mayo

--- src/app/category/category.component.ts
+++ src/app/category/category.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { Category } from 'src/app/modelo/category';
+
 
 @Component({
   selector: 'app-category',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
-
+  titulo : string = 'Categorias';
+  category : Category = new Category();
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  almacenarCat() : void{
+    Swal.fire({
+      title: 'Categoria almacenada',
+      text: 'La categoria se almaceno correctamente',
+      icon: 'success',
+      confirmButtonText: 'Aceptar'
+    });
   }
 
 }
---
+++ src/app/modelo/category.ts
@@ -0,0 +1,5 @@
+export class Category {
+    idCategoria : number=0;
+    nombreCategoria : string = '';
+    descripcionCategoria : string = '';
+}
