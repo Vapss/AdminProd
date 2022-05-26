005bc8f8d1b30bc953a11d284cf85af963f945f6
José Antonio Portuguez Vázquez <65097050+Vapss@users.noreply.github.com>
2022-05-26 11:58:09 -0500

    Clase Jue 26 May
    Clase Jue 26 May

--- src/app/category/listado/listado.component.ts
+++ src/app/category/listado/listado.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from 'src/app/modelo/category';
+import { CategoriaService } from 'src/app/servicios/categoria.service';
 
 @Component({
   selector: 'app-listado',
@@ -9,9 +10,12 @@ import { Category } from 'src/app/modelo/category';
 export class ListadoComponent implements OnInit {
   titulo: string = 'Listado de categorías';
   listaDeCategorias: Category[]= [];
-  constructor() { }
+  constructor(private servicio : CategoriaService) { }
 
   ngOnInit(): void {
+    this.servicio.getCategorias().subscribe(
+      (categorias) => this.listaDeCategorias = categorias
+    );
   }
 
 }
