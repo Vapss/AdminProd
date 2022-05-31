03374ac335f1e933b1db7be6e3617afda06dda88
José Antonio Portuguez Vázquez <65097050+Vapss@users.noreply.github.com>
2022-05-31 09:29:04 -0500

    XD
    XD

--- src/app/category/listado/listado.component.ts
+++ src/app/category/listado/listado.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from 'src/app/modelo/category';
 import { CategoriaService } from 'src/app/servicios/categoria.service';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-listado',
@@ -9,13 +11,66 @@ import { CategoriaService } from 'src/app/servicios/categoria.service';
 })
 export class ListadoComponent implements OnInit {
   titulo: string = 'Listado de categorías';
-  listaDeCategorias: Category[]= [];
+  listaDeCategorias: Category[]= [
+
+    {
+      idCategoria : 1,
+      nombreCategoria : 'yo1',
+      descripcionCategoria: 'ya'
+  
+    },
+    {
+      idCategoria : 2,
+      nombreCategoria : 'yo2',
+      descripcionCategoria: 'ya'
+  
+    },
+    {
+      idCategoria : 3,
+      nombreCategoria : 'yo3',
+      descripcionCategoria: 'ya'
+  
+    },
+    {
+      idCategoria : 4,
+      nombreCategoria : 'yo4',
+      descripcionCategoria: 'ya'
+  
+    }
+  ];
   constructor(private servicio : CategoriaService) { }
 
   ngOnInit(): void {
     this.servicio.getCategorias().subscribe(
       (categorias) => this.listaDeCategorias = categorias
     );
+  }
+
+  eliminar(categoria: Category): void {
+    Swal.fire({
+      title: 'Está seguro?',
+      text: `¿Seguro que desea eliminar la categoría ${categoria.nombreCategoria}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar!',
+      cancelButtonText: 'No, cancelar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+        this.servicio.eliminarCategoria(categoria.idCategoria).subscribe(
+          () => {
+            this.listaDeCategorias = this.listaDeCategorias.filter(c => c !== categoria)
+            Swal.fire(
+              'Eliminado!',
+              `La categoría ${categoria.nombreCategoria} ha sido eliminada con éxito.`,
+              'success'
+            )
+          }
+        );
+      }
+    })
   }
 
 }
--- src/app/servicios/categoria.service.ts
+++ src/app/servicios/categoria.service.ts
@@ -8,12 +8,34 @@ import { Category } from '../modelo/category';
   providedIn: 'root'
 })
 export class CategoriaService {
-  private urlEndPoint: string = 'http://localhost:8082/api/categorias';
+  private urlEndPoint: string = 'localhost:8082/api/producto';
   constructor(private http : HttpClient) { }
   private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'});
 
   getCategorias(): Observable<Category[]> {
     return this.http.get(this.urlEndPoint).pipe(
+      map(response => response as Category[])
+    );
+  }
+
+  eliminarCategoria(id: number): Observable<Category> {
+    return this.http.delete<Category>(`${this.urlEndPoint}/${id}`, {headers: this.httpHeaders});
+  }
+
+  leerCategoria(id: number): Observable<Category> {
+    return this.http.get<Category>(`${this.urlEndPoint}/${id}`);
+  }
+
+  crearCategoria(category: Category): Observable<Category> {
+    return this.http.post<Category>(this.urlEndPoint, category, {headers: this.httpHeaders});
+  }
+
+  actualizarCategoria(category: Category): Observable<Category> {
+    return this.http.put<Category>(`${this.urlEndPoint}/${category.idCategoria}`, category, {headers: this.httpHeaders});
+  }
+
+  buscarCategoria(term: string): Observable<Category[]> {
+    return this.http.get<Category[]>(`${this.urlEndPoint}/buscar/${term}`).pipe(
       map(response => response as Category[])
     );
   }
